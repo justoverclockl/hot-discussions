7ca30d074108e344e41e709788be910d00a49cee
Javier S. Lirola <6697559+jslirola@users.noreply.github.com>
2023-04-09 17:57:19 +0200

    Add settings to exclude closed discussions

--- js/src/common/components/hotDiscussionsWidget.js
+++ js/src/common/components/hotDiscussionsWidget.js
@@ -21,11 +21,12 @@ export default class hotDiscussionsWidget extends Widget {
 
     oncreate(vnode) {
         const limitHotDisc = app.forum.attribute('justoverclock-hot-discussions.discLimit') || 10;
+        const filterDiscussions = app.forum.attribute('justoverclock-hot-discussions.hideClosed') === "1" ? '-is:locked' : '';
         // get discussions object
         const hotDisc = app.store
             .find('discussions', {
                 sort: '-commentCount',
-                filter: { q: '-is:locked' }, // Exclude closed discussions
+                filter: { q: filterDiscussions }, // Exclude closed discussions
                 page: { limit: limitHotDisc },
             })
             .then((results) => {
